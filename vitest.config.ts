2c74e545b0d95f10ba039408d06223996f047356
ryu <114303361+ryuapp@users.noreply.github.com>
2025-07-09 03:51:06 +0900

    fix: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "node",
-    globals: true,
     coverage: {
       provider: "v8",
       enabled: true,
