5fe3baac59e749124424734768d189afe5358522
ryu <114303361+ryuapp@users.noreply.github.com>
2025-07-09 03:49:20 +0900

    chore: clean up

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    globals: true,
+    coverage: {
+      provider: "v8",
+      enabled: true,
+      reporter: ["text", "json", "html"],
+    },
+    testTimeout: 10000,
+  },
+});
